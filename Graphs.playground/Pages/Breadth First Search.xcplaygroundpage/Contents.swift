3aaa44e31750b85c1474e4dc38a76a0e349a43af
Rohan Bimal Raj <rohanbimalraj@gmail.com>
2024-09-21 18:13:34 +0530

    Added introduction

---
+++ Graphs.playground/Pages/Breadth First Search.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+/*: [< Previous](@previous)          [Home](Introduction)           [Next >](@next)
+ ## Breadth First Search
+ The following code demonstrates **BFS**
+ */
+
+import Foundation
+
+var greeting = "Hello, playground"
---
+++ Graphs.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+/*:
+ 
+ # Introduction
+ A graph is an ordered pair of a set of vertices and set of edges. Edges can be directed or undirected.\
+ 
+ > Make sure you render text as Markup\
+ > Also make sure that you reveal the sidebar
+ 
+ ## Graph Data Structures
+ - Edge List (Look up O(n))
+ - Adjacency Matrices (Look up O(1) but space inefficient)
+ - Adjacency List (Look up is fasr and space efficient)
+ 
+ ### Table Of Contents
+ - [Breadth First Search](Breadth%20First%20Search)
+ */
